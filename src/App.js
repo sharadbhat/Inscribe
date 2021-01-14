a0915a4a51e0c08eb88320e5778ad697186bc48a
sharadbhat <sharad.mbhat@gmail.com>
2021-01-14 17:01:14 +0530

    minor

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import { Layout } from 'antd';
 
-// CSS
-import "./App.css";
-import "antd/dist/antd.css";
+// Components
 import TextInput from './components/TextInput';
+import logo256 from './images/logo256.png';
+
+// CSS
+import './App.css';
+import 'antd/dist/antd.css';
 
 const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <Layout className="layout" style={{ height: "100vh" }}>
+    <Layout className='layout' style={{ height: '100vh' }}>
     <Header>
-      <div className="logo" />
+      <a href='/' title='Inscribe'>
+        <img src={logo256} alt='logo' width='40' />
+      </a>
     </Header>
     <Content style={{ padding: '0 50px' }}>
-      <div style={{ paddingTop: 20 }}>
-      <TextInput />
+      <div className='textInputDiv'>
+        <TextInput />
       </div>
     </Content>
     <Footer style={{ textAlign: 'center' }}>
       Created by Sharad Bhat
       <div>
-        Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+        Icons made by <a href='https://www.flaticon.com/authors/pixel-perfect' title='Pixel perfect'>Pixel perfect</a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a>
       </div>
     </Footer>
   </Layout>
