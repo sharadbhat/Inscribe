c036afae5728eeeefbc84136b9a5a03895bb0644
sharadbhat <sharad.mbhat@gmail.com>
2021-01-15 02:23:41 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-import { Layout } from 'antd';
+import { Layout } from 'antd'
 
 // Components
-import TextInput from './components/TextInput';
-import logo256 from './images/logo256.png';
+import Logo from './components/Logo'
+import Settings from './components/Settings'
+import TextInput from './components/TextInput'
+
+// Utils
+import { Provider } from './utils/Context'
 
 // CSS
-import './App.css';
-import 'antd/dist/antd.css';
+import './App.css'
+import 'antd/dist/antd.css'
 
-const { Header, Content, Footer } = Layout;
+const { Header, Content, Footer } = Layout
 
 function App() {
   return (
-    <Layout className='layout' style={{ height: '100vh' }}>
-    <Header>
-      <a href='/' title='Inscribe'>
-        <img src={logo256} alt='logo' width='40' />
-      </a>
-    </Header>
-    <Content style={{ padding: '0 50px' }}>
-      <div className='textInputDiv'>
-        <TextInput />
-      </div>
-    </Content>
-    <Footer style={{ textAlign: 'center' }}>
-      Created by <a href='https://www.github.com/sharadbhat'>Sharad Bhat</a>
-      <div>
-        Icons made by <a href='https://www.flaticon.com/authors/pixel-perfect' title='Pixel perfect'>Pixel perfect</a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a>
-      </div>
-    </Footer>
-  </Layout>
-  );
+    <Provider>
+      <Layout className='layout' style={{ height: '100vh' }}>
+        <Header>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Logo />
+            <Settings />
+          </div>
+        </Header>
+        <Content className='textInputContainer'>
+          <TextInput />
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Created by <a href='https://www.github.com/sharadbhat'>Sharad Bhat</a>
+          <br />
+          Icons by <a href='https://www.flaticon.com/authors/pixel-perfect' title='Pixel perfect'>Pixel perfect</a>
+        </Footer>
+      </Layout>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
